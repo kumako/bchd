e787912c7ffc00cf6f9ce66854ab2130f583710a
Youness SAHOUANE <dev@youness.org>
2025-01-02 12:04:36 -0500

    Add InsecureSkipVerify option

--- rpcclient/infrastructure.go
+++ rpcclient/infrastructure.go
@@ -1096,6 +1096,10 @@ type ConnConfig struct {
 	// the wire in cleartext.
 	DisableTLS bool
 
+	// InsecureSkipVerify specifies whether to verify the server's TLS
+	// certificate.  This option should only be used for testing.
+	InsecureSkipVerify bool
+
 	// Certificates are the bytes for a PEM-encoded certificate chain used
 	// for the TLS connection.  It has no effect if the DisableTLS parameter
 	// is true.
@@ -1195,7 +1199,8 @@ func newHTTPClient(config *ConnConfig) (*http.Client, error) {
 			pool := x509.NewCertPool()
 			pool.AppendCertsFromPEM(config.Certificates)
 			tlsConfig = &tls.Config{
-				RootCAs: pool,
+				RootCAs:            pool,
+				InsecureSkipVerify: config.InsecureSkipVerify,
 			}
 		}
 	}
